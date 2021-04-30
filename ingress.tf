db973aaf005d162a2bff349b23336ce17a12d667
butterkitty <butterkitty@users.noreply.github.com>
2021-04-30 09:37:35 -0400

    Made the ingress work with GKE
    The biggest problem was malformed tls certificates

--- ingress.tf
+++ ingress.tf
@@ -1,26 +1,27 @@
 #Ingress required in order to specify tls
 resource "kubernetes_ingress" "apache2_ingress" {
-  metadata {
-      name = "apache2-ingress"
-      annotations = {
-          "kubernetes.io/ingress.class" = "gce"
-      }
-  }    
-  spec {
-      rule {
-        http {
-            path {
-                path = "/*"
-                backend {
-                    service_name = kubernetes_service.koho-rescuityonline.metadata.0.name
-                    service_port = "8080"
+    metadata {
+        name = "apache2-ingress"
+        annotations = {
+            "kubernetes.io/ingress.class" = "gce"
+        }
+    }    
+    spec {
+        rule {
+            host = "koho.rescuityonline.com"
+            http {
+                path {
+                    path = "/*"
+                    backend {
+                        service_name = kubernetes_service.koho-rescuityonline.metadata.0.name
+                        service_port = "8080"
+                    }
                 }
             }
         }
-      }
-      tls {
-          hosts = ["koho.rescuityonline.com"]
-          secret_name = "koho-tls"
-      }
-  }
+        tls {
+            hosts = ["koho.rescuityonline.com"]
+            secret_name = "koho-tls"
+        }
+    }
 }
